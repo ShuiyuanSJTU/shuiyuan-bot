7f6f8234dd61160fbbe5821122ade90a8cdc8684
pangbo13 <51732678+pangbo13@users.noreply.github.com>
2024-11-25 17:19:03 +0000

    add stack info

--- backend/bot.py
+++ backend/bot.py
@@ -38,39 +38,5 @@ class BotManager:
                             return_values.append(action_return)
                 except Exception as e:
                     logging.error(
-                        f"Error when triggering event {event} for action {action_name}: {e}")
+                        f"Error when triggering event {event} for action {action_name}: {e}", exc_info=e)
         return return_values
-
-    # def __init__(self, api: BotAPI) -> None:
-    #     self.bot_api = api
-    #     self.bot_username: str = api.username
-    #     self.bot_actions: List[BotAction] = []
-
-    # def register_bot_action(self, bot_action: Type[BotAction]):
-    #     action = bot_action(self.bot_api,self.bot_username)
-    #     self.bot_actions.append(action)
-
-    # def respond(self,post: Post,allow_muiltiple_response=False):
-    #     for bot_action in self.bot_actions:
-    #         action_taken = bot_action.on_post_created(post)
-    #         if action_taken and not allow_muiltiple_response:
-    #             break
-
-    # def should_respond(self,post: Post,allow_muiltiple_response=False):
-    #     # ignore bot
-    #     if post.username == self.bot_username:
-    #         return False
-    #     # check if bot is mentioned
-    #     if re.search(rf"(^|\s)@{self.bot_username}\b", post.raw):
-    #         return True
-    #     # check if is reply to bot
-    #     reply_to_username = post.reply_to_user.username \
-    #             if post.reply_to_user is not None else None
-    #     if reply_to_username == self.bot_username:
-    #         return True
-    #     return False
-
-    # def on_post_created(self,post_data: dict):
-    #     post = Post(**post_data)
-    #     if self.should_respond(post):
-    #         self.respond(post)
