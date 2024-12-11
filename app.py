fcdb434826b7250f880aa223978a16cec71c1603
pangbo13 <51732678+pangbo13@users.noreply.github.com>
2024-12-11 06:18:39 +0000

    feat: supress access log

--- app.py
+++ app.py
@@ -5,10 +5,25 @@ from backend.bot_config import config as CONFIG
 from security import verify_discourse_webhook_request, verify_ip_address, verify_discourse_instance
 
 import eventlet
+import logging
 from eventlet import wsgi
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 
+logging.getLogger('werkzeug').setLevel(logging.WARNING)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler = RotatingFileHandler('app.warning.log', maxBytes=1024 * 1024, backupCount=2)
+file_handler.setLevel(logging.WARNING)
+file_handler.setFormatter(formatter)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.WARNING)
+
+root_logger = logging.getLogger()
+root_logger.addHandler(file_handler)
+root_logger.addHandler(console_handler)
 
 @app.before_request
 def verify_request():
@@ -39,4 +54,4 @@ def endpoint():
 
 if __name__ == "__main__":
     wsgi.server(eventlet.listen(
-        (CONFIG.server.bind_address, CONFIG.server.bind_port)), app)
+        (CONFIG.server.bind_address, CONFIG.server.bind_port)), app, log_output=False)
