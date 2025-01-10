157712ee4d9ad43c9067f6386239e827145fda6b
pangbo13 <51732678+pangbo13@users.noreply.github.com>
2025-01-11 01:45:22 +0800

    feat: enhance database management with scoped sessions and CRUD methods

--- backend/db.py
+++ backend/db.py
@@ -1,6 +1,6 @@
 import logging
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 from contextlib import contextmanager
 
 from .bot_config import config as Config
@@ -9,23 +9,59 @@ from .utils.singleton import Singleton
 
 logger = logging.getLogger(__name__)
 
-Base = declarative_base()
+_Base = declarative_base()
+
+class Base(_Base):
+    __abstract__ = True
+
+    @classmethod
+    def where(cls, **kwargs):
+        return db_manager._scoped_session().query(cls).filter_by(**kwargs)
+
+    @classmethod
+    def find(cls, **kwargs):
+        return db_manager._scoped_session().query(cls).filter_by(**kwargs).first()
+
+    def save(self):
+        session = db_manager._scoped_session()
+        session.merge(self)
+        session.commit()
+        return self
+
+    def delete(self):
+        session = db_manager._scoped_session()
+        session.delete(self)
+        session.commit()
+        return self
 
 @Singleton
 class DBManager:
     def __init__(self):
-        self.engine = create_engine(Config.db_url)
-        self.sessionmaker = sessionmaker(bind=self.engine)
+        self._engine = create_engine(Config.db_url)
+        self._sessionmaker = sessionmaker(bind=self._engine)
+        self._scoped_session = scoped_session(self._sessionmaker)
         if not is_running_under_pytest() and \
             ':memory:' in Config.db_url: # pragma: no cover
             logger.warning("Using in-memory database, data will be lost on restart")
 
     def init_tables(self):
-        Base.metadata.create_all(self.engine)
+        Base.metadata.create_all(self._engine)
+
+    @contextmanager
+    def scoped_session(self):
+        session = self._scoped_session()
+        try:
+            yield session
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            raise
+        finally:
+            self._scoped_session.remove()
 
     @contextmanager
     def session(self):
-        session = self.sessionmaker()
+        session = self._sessionmaker()
         try:
             yield session
             session.commit()
--- tests/test_db.py
+++ tests/test_db.py
@@ -2,6 +2,24 @@ import pytest
 from unittest.mock import patch, MagicMock
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import String, Integer, Column, inspect
+
+@pytest.fixture
+def scoped_session():
+    from backend.db import db_manager
+    with db_manager.scoped_session() as session:
+        yield session
+
+@pytest.fixture
+def test_table():
+    from backend.db import DBManager, Base
+    class TestTable(Base):
+        __tablename__ = 'test_table'
+        id = Column(Integer, primary_key=True)
+        name = Column(String, nullable=False)
+    db_manager = DBManager()
+    db_manager.init_tables()
+    yield TestTable
 
 @pytest.fixture(autouse=True)
 def auto_patch(patch_bot_config):
@@ -10,13 +28,13 @@ def auto_patch(patch_bot_config):
 def test_db_manager_initialization(patch_bot_config):
     from backend.db import DBManager
     db_manager = DBManager()
-    assert db_manager.engine.url == create_engine(patch_bot_config.db_url).url
-    assert isinstance(db_manager.sessionmaker, sessionmaker)
+    assert db_manager._engine.url == create_engine(patch_bot_config.db_url).url
+    assert isinstance(db_manager._sessionmaker, sessionmaker)
 
 def test_session_commit():
     from backend.db import DBManager, Base
     db_manager = DBManager()
-    db_manager.sessionmaker = MagicMock(return_value=MagicMock())
+    db_manager._sessionmaker = MagicMock(return_value=MagicMock())
     with db_manager.session() as session:
         session.add(MagicMock())
     session.commit.assert_called_once()
@@ -24,7 +42,7 @@ def test_session_commit():
 def test_session_rollback():
     from backend.db import DBManager
     db_manager = DBManager()
-    db_manager.sessionmaker = MagicMock(return_value=MagicMock())
+    db_manager._sessionmaker = MagicMock(return_value=MagicMock())
     with pytest.raises(Exception):
         with db_manager.session() as session:
             session.add(MagicMock())
@@ -33,11 +51,67 @@ def test_session_rollback():
 
 def test_init_tables():
     from backend.db import DBManager, Base
-    from sqlalchemy import String, Integer, Column, inspect
     class TestTable(Base):
         __tablename__ = 'test_table'
         id = Column(Integer, primary_key=True)
         name = Column(String)
     db_manager = DBManager()
     db_manager.init_tables()
-    assert 'test_table' in inspect(db_manager.engine).get_table_names()
+    assert 'test_table' in inspect(db_manager._engine).get_table_names()
+
+def test_base_find(test_table, scoped_session):
+    from backend.db import db_manager as DBManager
+    with DBManager.session() as session:
+        session.add(test_table(id=1, name='test'))
+    r = test_table.find(id=1)
+    assert r.name == 'test'
+    assert r.id == 1
+
+def test_base_where(test_table, scoped_session):
+    from backend.db import db_manager as DBManager
+    with DBManager.session() as session:
+        session.add(test_table(id=1, name='test'))
+        session.add(test_table(id=2, name='test'))
+    q = test_table.where(name='test')
+    r = q.all()
+    assert q.count() == 2
+    assert len(r) == 2
+    assert set([i.id for i in r]) == {1, 2}
+
+def test_base_add(test_table, scoped_session):
+    t = test_table(id=1, name='test')
+    t.save()
+    from backend.db import db_manager as DBManager
+    with DBManager.session() as session:
+        assert session.query(test_table).filter_by(id=1).first().name == 'test'
+
+def test_base_update(test_table, scoped_session):
+    from backend.db import db_manager as DBManager
+    with DBManager.session() as session:
+        session.add(test_table(id=1, name='test'))
+    t = test_table.find(id=1)
+    t.name = 'test2'
+    t.save()
+    with DBManager.session() as session:
+        assert session.query(test_table).filter_by(id=1).first().name == 'test2'
+
+def test_base_delete(test_table, scoped_session):
+    from backend.db import db_manager as DBManager
+    with DBManager.session() as session:
+        session.add(test_table(id=1, name='test'))
+    t = test_table.find(id=1)
+    t.delete()
+    with DBManager.session() as session:
+        assert session.query(test_table).filter_by(id=1).first() is None
+
+def test_scoped_session_rollback(test_table, scoped_session):
+    from backend.db import db_manager as DBManager
+    t1 = test_table(id=1, name=None)
+    t2 = test_table(id=2, name='test')
+    with pytest.raises(Exception):
+        with DBManager.scoped_session() as session:
+            t2.save()
+            t1.save()
+    with DBManager.session() as session:
+        assert session.query(test_table).filter_by(id=1).first() is None
+        assert session.query(test_table).filter_by(id=2).first().name == 'test'
